779d892801b8a9741026dea0a3499ff0758128e2
2mol <2mol@users.noreply.github.com>
2021-01-08 23:34:09 +0100

    giraffe hello-world

---
+++ webserver-giraffe-hello-world/Program.fs
@@ -0,0 +1,37 @@
+open Giraffe
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Hosting
+open Microsoft.AspNetCore.Http
+
+let webApp : HttpFunc -> HttpContext -> HttpFuncResult =
+    choose [
+        GET >=> choose [
+            route "/" >=> text "index"
+        ]
+        setStatusCode 404 >=> text "Not Found"
+    ]
+
+let configureApp (app : IApplicationBuilder) =
+    app.UseGiraffe webApp
+
+// let configureServices (services : IServiceCollection) =
+//     services.AddGiraffe() |> ignore
+
+[<EntryPoint>]
+let main args =
+    try
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(
+                fun webHost ->
+                    webHost
+                        .Configure(configureApp)
+                        // .ConfigureServices(configureServices)
+                        |> ignore
+                )
+            .Build()
+            .Run()
+        0
+    with ex ->
+        // Log.Emergency("Host terminated unexpectedly.", ex)
+        1
