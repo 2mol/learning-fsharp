19e7a3969b4d9f06563fed92c8cc1c4921f24a1d
2mol <2mol@users.noreply.github.com>
2021-02-26 15:01:06 +0100

    suave improvement

--- suave-hello-world/Program.fs
+++ suave-hello-world/Program.fs
@@ -2,18 +2,20 @@ open Suave
 open Suave.Filters
 open Suave.Operators
 open Suave.Successful
+open Suave.RequestErrors
 
 let app =
-  choose
-    [ GET >=> choose
-        [ path "/hello" >=> OK "Hello GET"
-          path "/goodbye" >=> OK "Good bye GET"
-        ]
-      POST >=> choose
-        [ path "/hello" >=> OK "Hello POST"
-          path "/goodbye" >=> OK "Good bye POST"
-        ]
-    ]
+  choose [
+    GET >=> choose [
+      path "/hello" >=> OK "Hello GET"
+      path "/goodbye" >=> OK "Good bye GET"
+      ]
+    POST >=> choose [
+      path "/hello" >=> OK "Hello POST"
+      path "/goodbye" >=> OK "Good bye POST"
+      ]
+    NOT_FOUND "Resource not found."
+  ]
 
 // let config = { defaultConfig with homeFolder = Some "./wwwroot" }
 
