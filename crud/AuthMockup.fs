1904865d487fe36cf0e4564314cee7bee2066b87
2mol <2mol@users.noreply.github.com>
2021-03-08 08:35:40 +0100

    doc comments update

--- crud/AuthMockup.fs
+++ crud/AuthMockup.fs
@@ -6,38 +6,32 @@ open Giraffe
 open Microsoft.AspNetCore.Http
 
 
-// let ensureAuthentication (token : string) : HttpHandler =
-//   fun (next : HttpFunc) (ctx : HttpContext) ->
-//     setStatusCode 401
+type Claims = {
+  UserId: Guid;
+  Email: string;
+  }
 
 let verifyUserToken (token : string) : Result<Guid, string> =
   Ok <| Guid.Parse "00000000-0000-0000-0000-000000000000"
 
-let verifyClaims (next : HttpFunc) (ctx : HttpContext) =
-  // ensure the user token is valid, effectively verifying the claims within
-  // this is good, but a function that would return the claims would be
+let ensureClaimsNaive (next : HttpFunc) (ctx : HttpContext) =
+  // Ensure the user token is valid, effectively verifying the claims within
+  // This is fine, but additionally returning the claims would be
   // better. It would force/nudge us to use this function whenever we want to
   // extract the claim data.
   task {
     match Result.bind verifyUserToken (ctx.GetRequestHeader "Authorization") with
     | Ok guid ->
-      // TODO: somehow return the data as well
       return! next ctx
     | Error err ->
       // TODO: log the error
       return! setStatusCode 401 earlyReturn ctx
   }
 
-type Claims = {
-  UserId: Guid;
-  Email: string;
-  }
-
 let ensureClaims
-  // ensure the user token is valid, effectively verifying the claims within
-  // this is good, but a function that would return the claims would be
-  // better. It would force/nudge us to use this function whenever we want to
-  // extract the claim data.
+  // Ensure the user token is valid, which verifies the claims and passes it to
+  // another handler function. This way we can have one wrapper for passing claims
+  // and can write our business logic without worrying about the authentication.
   (innerHandler : Claims -> HttpHandler)
   (next : HttpFunc)
   (ctx : HttpContext)
