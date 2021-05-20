a6375afce1535c5c816c900b34bedc0733f202ba
2mol <2mol@users.noreply.github.com>
2021-05-20 11:35:44 +0200

    update CRUD to Npgsql.Fsharp v4

--- crud-user-service/Database.fs
+++ crud-user-service/Database.fs
@@ -36,27 +36,45 @@ let connection =
     |> Sql.config "Pooling=true" // optional Config for connection string
 
 let getUser id =
-    connection
-    |> Sql.connectFromConfig
-    |> Sql.query "SELECT * FROM users where id = @userid"
-    |> Sql.parameters [ "@userid", Sql.uuid id ]
-    |> Sql.executeRow (fun read ->
-    {
-      Id = read.uuid "id"
-      Data = read.text "data" |> deserialize
-      CreatedAt = read.dateTime "created_at"
-    })
+    try
+        connection
+        |> Sql.connectFromConfig
+        |> Sql.query "SELECT * FROM users where id = @userid"
+        |> Sql.parameters [ "@userid", Sql.uuid id ]
+        |> Sql.executeRow (fun read ->
+        {
+          Id = read.uuid "id"
+          Data = read.text "data" |> deserialize
+          CreatedAt = read.dateTime "created_at"
+        })
+        |> Ok
+    with
+    | ex ->
+        printfn "Database error: \n%A" ex.Message
+        Error ()
 
 let addUser userData =
-    connection
-    |> Sql.connectFromConfig
-    |> Sql.query "insert into users (data) values (@data) returning id"
-    |> Sql.parameters [ "@data", Sql.jsonb <| serialize userData ]
-    |> Sql.executeRow (fun read -> read.uuid "id")
+    try
+        connection
+        |> Sql.connectFromConfig
+        |> Sql.query "insert into users (data) values (@data) returning id"
+        |> Sql.parameters [ "@data", Sql.jsonb <| serialize userData ]
+        |> Sql.executeRow (fun read -> read.uuid "id")
+        |> Ok
+    with
+    | ex ->
+        printfn "Database error: \n%A" ex.Message
+        Error ()
 
 let updateUser id userData =
-    connection
-    |> Sql.connectFromConfig
-    |> Sql.query "update users set data = @data where id = @userid"
-    |> Sql.parameters [ "@data", Sql.jsonb <| serialize userData; "@userid", Sql.uuid id ]
-    |> Sql.executeNonQuery
+    try
+        connection
+        |> Sql.connectFromConfig
+        |> Sql.query "update users set data = @data where id = @userid"
+        |> Sql.parameters [ "@data", Sql.jsonb <| serialize userData; "@userid", Sql.uuid id ]
+        |> Sql.executeNonQuery
+        |> Ok
+    with
+    | ex ->
+        printfn "Database error: \n%A" ex.Message
+        Error ()
