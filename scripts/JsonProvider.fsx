e77b7cd17193653fd5a5f5f4b6121b7e1555b44d
2mol <2mol@users.noreply.github.com>
2021-05-22 19:15:59 +0200

    improve

--- scripts/JsonProvider.fsx
+++ scripts/JsonProvider.fsx
@@ -5,3 +5,15 @@ open FSharp.Data
 type Pokemon = JsonProvider<"https://pokeapi.co/api/v2/pokemon/1">
 
 let bulbasaur = Pokemon.GetSample()
+
+
+let pokeApiRoot = "https://pokeapi.co/api/v2/pokemon"
+
+let genOne = [ for index in 1..150 do Pokemon.Load($"{pokeApiRoot}/{index}") ]
+    // There are better ways to get the original 150 but this works for our demo
+    // https://pokeapi.co/api/v2/pokemon?limit=150
+
+let tallestPokemonInGenOne =
+    genOne
+    |> List.sortByDescending (fun pokemon -> pokemon.Height)
+    |> List.truncate 15
